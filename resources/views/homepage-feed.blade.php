a8d07b47aae34f0eb7f9b650a2322e0866611b86
Marcos Endler <m.endler@hotmail.com>
2023-06-05 00:17:02 -0300

    posts feed

--- resources/views/homepage-feed.blade.php
+++ resources/views/homepage-feed.blade.php
@@ -10,9 +10,12 @@
           </a>
         @endforeach
       </div>
-          
-      @else
+          <div class="mt-4">
+            {{$posts->links()}}
+          </div>
+      
 
+      @else
         <div class="text-center">
           <h2>Hello <strong>{{auth()->user()->username}}</strong>, your feed is empty.</h2>
           <p class="lead text-muted">Your feed displays the latest posts from the people you follow. If you don&rsquo;t have any friends to follow that&rsquo;s okay; you can use the &ldquo;Search&rdquo; feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
